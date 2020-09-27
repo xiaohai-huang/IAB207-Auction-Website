14f7fa875b584225bedcba89d39d866f9cf0c460
Casey-QUT <71915840+Casey-QUT@users.noreply.github.com>
2020-09-28 08:55:06 +1000

    Create items.py

---
+++ auction/items.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+from flask import request
+from flask import session
+from flask import redirect, url_for
+from flask import render_template
+from werkzeug.utils import secure_filename
+import os
+from .models import Item, Wish, Watchlist, Bid, User
+from . import db
+
+bp = Blueprint('item', __name__, url_prefix='/items')
+
+@bp.route('/<id>')
+def show(id):
+    item = Item.query.filter_by(id=id).first()
+    # basically, using id as the url and querying the database with it i.e. books.com/items/1 would return the page of item with id 1
+    
