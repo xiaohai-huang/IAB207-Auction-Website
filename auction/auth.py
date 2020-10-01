4f78eeaf2f8fa8e7cd49e5821682bacb046909f5
xiaohai-huang <n10172912@qut.edu.au>
2020-10-02 00:35:18 +1000

    fixed bugs on nav bar

--- auction/auth.py
+++ auction/auth.py
@@ -2,7 +2,6 @@ from flask import (
     Blueprint, flash, render_template, request, url_for, redirect
 ) 
 from werkzeug.security import generate_password_hash,check_password_hash
-#from .models import User
 from .forms import LoginForm,RegisterForm
 from flask_login import login_user, login_required,logout_user
 from auction import db
