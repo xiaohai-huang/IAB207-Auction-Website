b152ace8da02be7a011708903ba5e95e9da24ec7
voidify3 <sophiawalshlong@gmail.com>
2020-10-01 13:07:04 +1000

    added index(), minor changes to base.html

--- auction/views.py
+++ auction/views.py
@@ -1,12 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, session
 from flask_login import current_user
+from .models import Item
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    if current_user.is_authenticated!=False:
-
-        return f'<h1>{current_user.username}<h1>'
-    else:
-        return 'welcome, nobody'
+    #items = Item.query.all()
+    return render_template('index.html') #,items=items)
+    #Must implement the db so that this can be fixed
