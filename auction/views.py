9b6cc9a20785e12b00dcc7449ec558d330af09f3
Casey-QUT <71915840+Casey-QUT@users.noreply.github.com>
2020-10-06 11:15:54 +1000

    Update views.py
    index.html now pulls in database data to populate cards

--- auction/views.py
+++ auction/views.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, render_template, request, session
 from flask_login import current_user
-from .models import Item
+from .models import Item, Bid
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    #items = Item.query.all()
-    return render_template('index.html') #,items=items)
-    #TODO: implement the db so that this can be fixed
+    bid_counts = Bid.query.all() # is there a better way to do this than querying the entire bid table? :(
+    items = Item.query.order_by(Item.item_datetime.desc()).limit(5).all() # grabs the 5 most recent as per assignment specs
+    return render_template('index.html', items = items, bid_counts = bid_counts)
 
 #TODO: search route like in the workshop project
 
