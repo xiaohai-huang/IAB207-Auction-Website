892cfb05d0922f18e7ca30ff97ded56e0ba2783a
Casey-QUT <71915840+Casey-QUT@users.noreply.github.com>
2020-10-08 11:01:56 +1000

    Update items.py
    item/<id> now works and show.html is now supplied with necessary data.
    double check the bidders SQL query is correct - it seems to be working but I struggle with joins so look out for that hahaha

--- auction/items.py
+++ auction/items.py
@@ -12,11 +12,13 @@ bp = Blueprint('item', __name__, url_prefix='/items')
 
 @bp.route('/<id>')
 def show(id):
-    #item = Item.query.filter_by(id=id).first()
-    # basically, using id as the url and querying the database with it i.e. books.com/items/1 would return the page of item with id 1
-    # put form here
-    # render template goes here
-    return render_template("item/show.html")#,item=item, form=form, etc)
+    item = Item.query.filter_by(id=id).first() # grab item query data by id
+    image = Image.query.filter_by(item_id = id).all()
+    user = User.query.filter_by(id = item.user_id).first() # this is the user who has listed the item, need to pull their username
+    bids = Bid.query.filter_by(item_id = item.id).order_by(Bid.bid_datetime.desc()).limit(10).all() # grabs top ten most recent bids
+    formatted_datetime = item.item_datetime.strftime("%m/%d/%Y, %H:%M:%S") # formats into a nice datetime that can be used to show item listing date; this is optional
+    bidders = db.session.query(User).join(Bid, User.id == Bid.user_id, isouter=True).join(Item, id == Bid.item_id, isouter=True).filter_by(id = Bid.item_id).order_by(Bid.bid_datetime.desc()).group_by(User.id).limit(10).all()
+    return render_template("item/show.html", item = item, image = image, user=user, bids = bids, formatted_datetime = formatted_datetime, bidders = bidders)#,item=item, form=form, etc)
 
 @bp.route('/create', methods=['GET', 'POST'])
 #TODO: MAKE THE NECESSARY CHANGES TO UNCOMMENT THE COMMENTED CODE
