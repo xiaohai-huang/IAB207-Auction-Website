74645380cabc63ef644bc7244603d66ec3e3f93c
xiaohai-huang <n10172912@qut.edu.au>
2020-10-10 18:32:19 +1000

    Require 3 more attributes to register new user

--- auction/auth.py
+++ auction/auth.py
@@ -2,7 +2,7 @@ from flask import (
     Blueprint, flash, render_template, request, url_for, redirect
 ) 
 from werkzeug.security import generate_password_hash,check_password_hash
-from .forms import LoginForm,RegisterForm
+from auction.forms import LoginForm,RegisterForm
 from flask_login import login_user, login_required,logout_user
 from auction import db
 from auction.models import User
@@ -46,6 +46,9 @@ def register():
             uname =register.user_name.data
             pwd = register.password.data
             email=register.email_id.data
+            user_type = register.user_type.data
+            contact_num = register.contact_number.data
+            address = register.address.data
             #check if a user exists
             u1 = User.query.filter_by(username=uname).first()
             if u1:
@@ -54,7 +57,8 @@ def register():
             # don't store the password - create password hash
             pwd_hash = generate_password_hash(pwd)
             #create a new user model object
-            new_user = User(username=uname, password_hash=pwd_hash, email_address=email)
+            new_user = User(username=uname, password_hash=pwd_hash, email_address=email,
+                            contact_number=contact_num ,user_type=user_type,address=address)
             db.session.add(new_user)
             db.session.commit()
             #commit to the database and redirect to HTML page
